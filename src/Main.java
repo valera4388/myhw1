5fcd1680259ad6d1085403ae5901a12f301e5b35
mikhalev valera <mihalev.valera@mail.ru>
2024-08-11 14:36:36 +0300

    add prog commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int price = 69842; //стоимость билета
+        int priceBonus = 20; //сумма для получения 1 мили
+
+        int bonus = price / priceBonus;
+        System.out.println("Количество начисленных миль: " + bonus);
+
     }
 }
